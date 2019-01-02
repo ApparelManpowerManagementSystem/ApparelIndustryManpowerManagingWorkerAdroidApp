d6024360e4b7faa65ceaecf07b136e3b9bb89ec5
Pasindu Madusanka <pasindusenerath@gmail.com>
2019-01-02 16:46:31 +0530

    Update manpower.sql

--- db_sql/manpower.sql
+++ db_sql/manpower.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 02, 2019 at 10:51 AM
+-- Generation Time: Jan 02, 2019 at 11:14 AM
 -- Server version: 5.7.21
 -- PHP Version: 7.2.4
 
